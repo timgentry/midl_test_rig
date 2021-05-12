952b8a2a2283f55de7f4ab6764be6cb88b2654bc
Tim Gentry <timgentry@users.noreply.github.com>
2021-05-12 23:39:24 +0100

    Scaffolded code

---
+++ lib/midl_test_rig.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "midl_test_rig/version"
+
+module MidlTestRig
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/midl_test_rig/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MidlTestRig
+  VERSION = "0.1.0"
+end
